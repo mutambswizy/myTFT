c01dfbb5164429c3291e70d86c683e6b7d369ccf
wisdom <mutambswizy@gmail.com>
2022-06-08 19:21:17 +0200

    102 ohs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello");
-        System.out.println("wisdom");
+System.out.println("hello");
+    // my comments
+
 
     }
 }
